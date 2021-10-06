171cc662c739697d109afef43802ff33792a9cac
malicious233 <alfons.hedstrom@gmail.com>
2021-10-06 10:14:14 +0200

    - Standard character controller set up, using a movement script and an inputmanager It uses the 3D character controller, even though the game is 2D

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    ///Handles the players input
+    
     public Vector2 inputVector;
+    public bool jumpInput;
 
     public void Update()
     {
         inputVector.x = Input.GetAxisRaw("Horizontal");
         inputVector.y = Input.GetAxisRaw("Vertical");
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
     }
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    InputManager input;
+    #region Variables
 
     [Header("Ground Stats:")]
     [SerializeField] private float moveSpeed;
@@ -12,16 +12,74 @@ public class PlayerMovement : MonoBehaviour
     [Header("Aerial Stats:")]
     [SerializeField] private float jumpSpeed;
     [SerializeField] private float gravity;
+    [SerializeField] private float maxFallSpeed;
+
+    #endregion
 
 
-    // Update is called once per frame
-    void Update()
+    #region Private Variables
+
+    private Vector2 velocity;
+    private bool grounded;
+
+    InputManager input;
+    CharacterController controller;
+
+    #endregion
+
+    #region Unity Methods
+    public void Update()
     {
         
+        Movement(input.inputVector.x);
+        CanJump(input.jumpInput);
+        AddGravity();
+        ApplyMovement();
     }
 
     public void Awake()
     {
         input = GetComponent<InputManager>();
+        controller = GetComponent<CharacterController>();
     }
+
+    #endregion
+
+    #region Methods
+    public void Movement(float _verticalInput)
+        ///Sets horizontal velocity depending on given input
+    {
+        velocity.x = _verticalInput * moveSpeed; 
+    }
+
+    private void AddGravity()
+        ///Adds downwards velocity if not grounded
+    {
+        if (!controller.isGrounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+    }
+
+    public void CanJump(bool _jumpInput)
+        ///Jump.. from the ground
+    {
+        if (_jumpInput)
+        {
+            velocity.y = jumpSpeed;
+        }
+        
+    }
+
+    private void ApplyMovement()
+        ///Applies velocity as controller movement. This method is put late in the update method
+    {
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    #endregion
 }
