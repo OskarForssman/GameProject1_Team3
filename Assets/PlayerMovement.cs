41a4dce0197e76d6621f07912860145af7399ec8
malicious233 <alfons.hedstrom@gmail.com>
2021-10-06 10:55:09 +0200

    Some improved groundcheck mechanics, still not good enough

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,26 +14,43 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float gravity;
     [SerializeField] private float maxFallSpeed;
 
+    [SerializeField] private LayerMask groundLayerMask; 
+
     #endregion
 
 
     #region Private Variables
 
-    private Vector2 velocity;
+    private Vector2 velocity; 
     private bool grounded;
+
+    #region References
 
     InputManager input;
     CharacterController controller;
 
     #endregion
 
+    #endregion
+
     #region Unity Methods
+
+    
     public void Update()
     {
         
         Movement(input.inputVector.x);
-        CanJump(input.jumpInput);
-        AddGravity();
+
+        grounded = GroundCheck();
+        if (grounded)
+        {
+            CanJump(input.jumpInput);
+        }
+        else
+        {
+            AddGravity();
+        }
+
         ApplyMovement();
     }
 
@@ -43,9 +60,36 @@ public class PlayerMovement : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
+    public void OnDrawGizmos()
+    {
+        CharacterController bruh = GetComponent<CharacterController>();
+        float offset = bruh.height / 2 - bruh.radius / 2;
+        Vector3 sphereLocation = transform.position + Vector3.down * offset;
+        Gizmos.DrawWireSphere(sphereLocation, bruh.radius);
+
+    }
+
     #endregion
 
     #region Methods
+
+    public bool GroundCheck()
+        ///Returns a bool by spherecasting beneath the charactercontroller to more elegantly groundcheck 
+    {
+        Ray downRay = new Ray(transform.position, Vector3.down);
+        if (Physics.SphereCast(downRay, controller.radius, controller.height/2, groundLayerMask)) //Projects a sphere straight down at the bottom of the controller collider
+        {
+            Debug.Log("Ground!");
+            return true;
+            
+        }
+        else
+        {
+            Debug.Log("Air!");
+            return false;
+        }
+    }
+
     public void Movement(float _verticalInput)
         ///Sets horizontal velocity depending on given input
     {
@@ -55,7 +99,7 @@ public class PlayerMovement : MonoBehaviour
     private void AddGravity()
         ///Adds downwards velocity if not grounded
     {
-        if (!controller.isGrounded)
+        if (!grounded)
         {
             velocity.y -= gravity * Time.deltaTime;
         }
@@ -66,7 +110,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void CanJump(bool _jumpInput)
-        ///Jump.. from the ground
+        ///Jumps.. from the ground, if the given jumpInput is true
     {
         if (_jumpInput)
         {
@@ -76,7 +120,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void ApplyMovement()
-        ///Applies velocity as controller movement. This method is put late in the update method
+        ///Applies velocity via the characterController move method. This method is put late in the update method
     {
         controller.Move(velocity * Time.deltaTime);
     }
