25578bcbd63b9f427169b0318f2d0c1c84da351f
malicious233 <alfons.hedstrom@gmail.com>
2021-10-11 11:06:04 +0200

    - Work on bubble collision shenanigans

--- Assets/BreakBubble.cs
+++ Assets/BreakBubble.cs
@@ -9,7 +9,7 @@ public class BreakBubble : MonoBehaviour
     [SerializeField] float colliderRadius;
     [SerializeField] bool willTrap; //Wether or not this bubble will trap hit enemies
 
-
+    /*
     public void Update()
     {
         //Destroy(gameObject, bubblepopinthisamountofsec);
@@ -42,7 +42,38 @@ public class BreakBubble : MonoBehaviour
             }
         }
     }
+    */
 
+    public void OnCollisionEnter(Collision collision)
+    {
+            if (!willTrap)
+            {
+                Stats s = collision.transform.GetComponent<Stats>();
+            if (s != null)
+            {
+                if (!s.isResistant)
+                {
+                    s.TakeDamage(1);
+                    Debug.Log("hit enemy");
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    Debug.Log("couldnt hit enemy");
+                    Destroy(gameObject);
+                }
+            }
+            if (willTrap)
+            {
+                //Insert code that traps the enemy inside the bubble
+
+            }
+
+
+        }
+
+            
+    }
     public void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, colliderRadius);
