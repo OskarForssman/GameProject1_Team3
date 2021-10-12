9be9c10c1cca90d2acb2a199c3f5cd9961dd304f
malicious233 <alfons.hedstrom@gmail.com>
2021-10-12 11:27:28 +0200

    - Fully implemented Contact damage to all enemies - Can customize the contactdamage zones

--- Assets/ContactDamage.cs
+++ Assets/ContactDamage.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ContactDamage : MonoBehaviour
 {
+    Stats stats;
     [SerializeField] float colliderRadius;
     [SerializeField] float colliderOffsetX;
     [SerializeField] float colliderOffsetY;
@@ -17,17 +18,28 @@ public class ContactDamage : MonoBehaviour
 
     public void Update()
     {
-        RaycastHit hit;
-
-        if (Physics.SphereCast(transform.position + transform.right * colliderOffsetX, colliderRadius, -transform.right, out hit, dist, playerLayerMask))
+        if (!stats.isTrapped)
         {
-
+            RaycastHit hit;
+            Vector3 yOffset = new Vector2(0, colliderOffsetY);
+            if (Physics.SphereCast(transform.position + transform.right * colliderOffsetX + yOffset, colliderRadius, -transform.right, out hit, dist, playerLayerMask))
+            {
+                Stats s = hit.transform.GetComponent<Stats>();
+                s.TakeDamage(1);
+            }
         }
+        
     }
 
     public void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(transform.position + transform.right * colliderOffsetX, colliderRadius);
-        Gizmos.DrawWireSphere(transform.position - transform.right * colliderOffsetX, colliderRadius);
+        Vector3 yOffset = new Vector2(0, colliderOffsetY);
+        Gizmos.DrawWireSphere(transform.position + transform.right * colliderOffsetX + yOffset, colliderRadius);
+        Gizmos.DrawWireSphere(transform.position - transform.right * colliderOffsetX + yOffset, colliderRadius);
+    }
+
+    public void Awake()
+    {
+        stats = GetComponent<Stats>();
     }
 }
