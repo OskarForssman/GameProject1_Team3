1fa00628af3e536d4d32d5eeed84c615f85c4a41
OskarForssman <38101758+OskarForssman@users.noreply.github.com>
2021-10-13 17:37:04 +0200

    Update Jumpthrough

--- Assets/JumpThroughScript.cs
+++ Assets/JumpThroughScript.cs
@@ -25,12 +25,14 @@ public class JumpThroughScript : MonoBehaviour
         
         if (jump>0)
         {
-            gameObject.layer = 13;
+            
+            collider.enabled = false;
         }
         //else the collision will not be ignored
         else
         {
-            gameObject.layer = 6;
+           
+            collider.enabled = true;
         }
     }
 }
